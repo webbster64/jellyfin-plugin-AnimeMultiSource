99cc01271c73e5fd01a006be90437b14fef2a2a4
webbster64 <arthur@blackcoffeedigital.co.za>
2025-12-18 10:17:20 +0200

    fix names for links

--- Jellyfin.Plugin.AnimeMultiSource/Providers/ExternalIds/AnimeMultiSourceExternalUrlProvider.cs
+++ Jellyfin.Plugin.AnimeMultiSource/Providers/ExternalIds/AnimeMultiSourceExternalUrlProvider.cs
@@ -7,37 +7,64 @@ using MediaBrowser.Model.Entities;
 
 namespace Jellyfin.Plugin.AnimeMultiSource.Providers.ExternalIds
 {
-    public class AnimeMultiSourceExternalUrlProvider : IExternalUrlProvider
+    public class AniDbExternalUrlProvider : IExternalUrlProvider
     {
-        public string Name => "Anime Multi-Source";
+        public string Name => "AniDB";
 
         public IEnumerable<string> GetExternalUrls(BaseItem item)
         {
-            // AniDB
             if (item.TryGetProviderId(Constants.AniDbProviderId, out var aniDbId))
             {
                 yield return $"https://anidb.net/anime/{aniDbId}";
             }
+        }
+    }
 
-            // AniList
+    public class AniListExternalUrlProvider : IExternalUrlProvider
+    {
+        public string Name => "AniList";
+
+        public IEnumerable<string> GetExternalUrls(BaseItem item)
+        {
             if (item.TryGetProviderId(Constants.AniListProviderId, out var aniListId))
             {
                 yield return $"https://anilist.co/anime/{aniListId}";
             }
+        }
+    }
 
-            // AniSearch
+    public class AniSearchExternalUrlProvider : IExternalUrlProvider
+    {
+        public string Name => "AniSearch";
+
+        public IEnumerable<string> GetExternalUrls(BaseItem item)
+        {
             if (item.TryGetProviderId(Constants.AniSearchProviderId, out var aniSearchId))
             {
                 yield return $"https://www.anisearch.com/anime/{aniSearchId}";
             }
+        }
+    }
 
-            // Kitsu
+    public class KitsuExternalUrlProvider : IExternalUrlProvider
+    {
+        public string Name => "Kitsu";
+
+        public IEnumerable<string> GetExternalUrls(BaseItem item)
+        {
             if (item.TryGetProviderId(Constants.KitsuProviderId, out var kitsuId))
             {
                 yield return $"https://kitsu.app/anime/{kitsuId}";
             }
+        }
+    }
 
-            // MyAnimeList
+    public class MalExternalUrlProvider : IExternalUrlProvider
+    {
+        public string Name => "MyAnimeList";
+
+        public IEnumerable<string> GetExternalUrls(BaseItem item)
+        {
             if (item.TryGetProviderId(Constants.MalProviderId, out var malId))
             {
                 yield return $"https://myanimelist.net/anime/{malId}";
